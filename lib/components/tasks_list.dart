2938ff9cfd09d43a908349f2517c0c5fd26341ac
parksangryong <parksangryong@Mac-mini-2.local>
2024-08-28 10:20:07 +0900

    listView.builder 사용

--- lib/components/tasks_list.dart
+++ lib/components/tasks_list.dart
@@ -1,14 +1,34 @@
 import 'package:flutter/material.dart';
 import 'package:todayey/components/tasks_tile.dart';
 
-class TasksList extends StatelessWidget {
+import '../models/task.dart';
+
+class TasksList extends StatefulWidget {
   @override
-  Widget build(BuildContext context) {
-    return ListView(children: [
-      TasksTile(check: false, onPress: () {}, title: 'title'),
-      TasksTile(check: false, onPress: () {}, title: 'title'),
-      TasksTile(check: false, onPress: () {}, title: 'title')
-    ]);
-  }
+  State<TasksList> createState() => _TasksListState();
 }
 
+class _TasksListState extends State<TasksList> {
+  List<Task> tasks = [
+    Task(name: 'Buy Milk'),
+    Task(name: 'Buy Apple'),
+    Task(name: 'Buy Pen'),
+  ];
+
+  @override
+  Widget build(BuildContext context) {
+    return ListView.builder(
+        itemCount: tasks.length,
+        itemBuilder: (context, index) {
+          return TasksTile(
+            taskTitle: tasks[index].name,
+            isChecked: tasks[index].isDone,
+            checkboxCallback: (checkboxState){
+              setState(() {
+                tasks[index].toggleDone();
+              });
+            },
+          );
+        });
+  }
+}
--- lib/components/tasks_tile.dart
+++ lib/components/tasks_tile.dart
@@ -1,43 +1,22 @@
 import 'package:flutter/material.dart';
 
-class TasksTile extends StatefulWidget {
-  final VoidCallback onPress;
-  final String title;
-  bool check;
+class TasksTile extends StatelessWidget {
+  final bool isChecked;
+  final String taskTitle;
+  final Function(bool?) checkboxCallback;
 
   TasksTile({
     super.key,
-    required this.check,
-    required this.onPress,
-    required this.title,
+    required this.taskTitle,
+    required this.isChecked,
+    required this.checkboxCallback
   });
 
   @override
-  _TasksTileState createState() => _TasksTileState();
-}
-
-class _TasksTileState extends State<TasksTile> {
-  @override
   Widget build(BuildContext context) {
     return ListTile(
-      onTap: widget.onPress,
-      title: Text(
-        widget.title,
-        style: const TextStyle(
-          color: Colors.black,
-          fontSize: 18,
-          fontWeight: FontWeight.w500,
-        ).copyWith(
-          decoration: widget.check ? TextDecoration.lineThrough : TextDecoration.none,)
-      ),
-      trailing: Checkbox(
-        value: widget.check,
-        onChanged: (value) {
-          setState(() {
-            widget.check = value!;
-          });
-        },
-      ),
+      title: Text(taskTitle, style: TextStyle(decoration: isChecked ? TextDecoration.lineThrough : null)),
+      trailing: Checkbox(value: isChecked, onChanged: checkboxCallback)
     );
   }
 }
